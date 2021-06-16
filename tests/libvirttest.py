1fcfc4a069ab9f6e60d55039e7602467936b4cf0
Pavel Hrdina <phrdina@redhat.com>
2021-06-16 13:51:26 +0200

    tests: report proper error if `abs_top_builddir` is not defined
    To run our test suite we require `abs_top_builddir` to be defined
    because we look there for the compiled libvirt-dbus binary.
    
    When the tests are executed directly it will error out without any
    reasonable message.
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

--- tests/libvirttest.py
+++ tests/libvirttest.py
@@ -11,7 +11,11 @@ import xmldata
 
 
 root = os.environ.get('abs_top_builddir')
-exe = os.path.join(root, 'src', 'libvirt-dbus')
+if root:
+    exe = os.path.join(root, 'src', 'libvirt-dbus')
+else:
+    raise Exception('To run tests use "meson tests" or "run" helper.')
+
 
 DBusGMainLoop(set_as_default=True)
 
