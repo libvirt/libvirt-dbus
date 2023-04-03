libvirt/libvirt-dbus

commit 87b16e9ee0df10290603ffdf61458ca44490470e

Implement GetBlockInfo, SetBlockThreshold methods and BlockThreshold signal for Domain Interface
Signed-off-by: Sergey Aganin <s.aganin@tionix.ru>

src/events.c
@@ -545,6 +545,32 @@ virtDBusEventsDomainDiskChange(virConnectPtr connection G_GNUC_UNUSED,
 }
 
 static gint
+virtDBusEventsDomainBlockThreshold(virConnectPtr connection G_GNUC_UNUSED,
+                                   virDomainPtr domain,
+                                   const gchar *device,
+                                   const gchar *devPath,
+                                   gint64 threshold,
+                                   gint64 excess,
+                                   gpointer opaque)
+{
+    virtDBusConnect *connect = opaque;
+    g_autofree gchar *path = NULL;
+
+    path = virtDBusUtilBusPathForVirDomain(domain, connect->domainPath);
+
+    g_dbus_connection_emit_signal(connect->bus,
+                                  NULL,
+                                  path,
+                                  VIRT_DBUS_DOMAIN_INTERFACE,
+                                  "BlockThreshold",
+                                  g_variant_new("(sstt)", device, devPath,
+                                                threshold, excess),
+                                  NULL);
+
+    return 0;
+}
+
+static gint
 virtDBusEventsNetworkEvent(virConnectPtr connection G_GNUC_UNUSED,
                            virNetworkPtr network,
                            gint event,
@@ -824,6 +850,10 @@ virtDBusEventsRegister(virtDBusConnect *connect)
     virtDBusEventsRegisterDomainEvent(connect,
                                       VIR_DOMAIN_EVENT_ID_WATCHDOG,
                                       VIR_DOMAIN_EVENT_CALLBACK(virtDBusEventsDomainWatchdog));
+
+    virtDBusEventsRegisterDomainEvent(connect,
+                                      VIR_DOMAIN_EVENT_ID_BLOCK_THRESHOLD,
+                                      VIR_DOMAIN_EVENT_CALLBACK(virtDBusEventsDomainBlockThreshold));                                      
 
     virtDBusEventsRegisterNetworkEvent(connect,
                                        VIR_NETWORK_EVENT_ID_LIFECYCLE,

data/org.libvirt.Domain.xml
@@ -183,6 +183,13 @@
       <arg name="flags" type="u" direction="in"/>
       <arg name="BlkioParameters" type="a{sv}" direction="out"/>
     </method>
+    <method name="GetBlockInfo">
+      <annotation name="org.gtk.GDBus.DocString"
+        value="See https://libvirt.org/html/libvirt-libvirt-domain.html#virDomainGetBlockInfo"/>
+      <arg name="disk" type="s" direction="in"/>
+      <arg name="flags" type="u" direction="in"/>
+      <arg name="blockInfo" type="(ttt)" direction="out"/>
+    </method>
     <method name="GetBlockIOTune">
       <annotation name="org.gtk.GDBus.DocString"
         value="See https://libvirt.org/html/libvirt-libvirt-domain.html#virDomainGetBlockIoTune"/>
@@ -510,6 +517,13 @@
       <arg name="params" type="a{sv}" direction="in"/>
       <arg name="flags" type="u" direction="in"/>
     </method>
+    <method name="SetBlockThreshold">
+      <annotation name="org.gtk.GDBus.DocString"
+        value="See https://libvirt.org/html/libvirt-libvirt-domain.html#virDomainSetBlockThreshold"/>
+      <arg name="dev" type="s" direction="in"/>
+      <arg name="threshold" type="t" direction="in"/>
+      <arg name="flags" type="u" direction="in"/>
+    </method>
     <method name="SetGuestVcpus">
       <annotation name="org.gtk.GDBus.DocString"
           value="See https://libvirt.org/html/libvirt-libvirt-domain.html#virDomainSetGuestVcpus"/>
@@ -748,6 +762,14 @@
       <annotation name="org.gtk.GDBus.DocString"
         value="See https://libvirt.org/html/libvirt-libvirt-domain.html#virConnectDomainEventWatchdogCallback"/>
       <arg name="action" type="i"/>
+    </signal>
+    <signal name="BlockThreshold">
+      <annotation name="org.gtk.GDBus.DocString"
+        value="See https://libvirt.org/html/libvirt-libvirt-domain.html#virConnectDomainEventBlockThresholdCallback"/>
+      <arg name="device" type="s"/>
+      <arg name="devPath" type="s"/>
+      <arg name="threshold" type="t"/>
+      <arg name="excess" type="t"/>
     </signal>
   </interface>
 </node>

src/domain.c
@@ -946,6 +946,34 @@ virtDBusDomainGetBlockIOParameters(GVariant *inArgs,
 }
 
 static void
+virtDBusDomainGetBlockInfo(GVariant *inArgs,
+                           GUnixFDList *inFDs G_GNUC_UNUSED,
+                           const gchar *objectPath,
+                           gpointer userData,
+                           GVariant **outArgs,
+                           GUnixFDList **outFDs G_GNUC_UNUSED,
+                           GError **error)
+{
+    virtDBusConnect *connect = userData;
+    g_autoptr(virDomain) domain = NULL;
+    virDomainBlockInfo info;
+    const gchar *disk;
+    guint flags;
+    
+    g_variant_get(inArgs, "(&su)", &disk, &flags);
+
+    domain = virtDBusDomainGetVirDomain(connect, objectPath, error);
+    if (!domain)
+        return; 
+
+    if (virDomainGetBlockInfo(domain, disk, &info, flags) < 0)
+        return virtDBusUtilSetLastVirtError(error);  
+
+    *outArgs = g_variant_new("((ttt))", info.capacity, info.allocation, 
+                             info.physical);
+}
+
+static void
 virtDBusDomainGetBlockIOTune(GVariant *inArgs,
                              GUnixFDList *inFDs G_GNUC_UNUSED,
                              const gchar *objectPath,
@@ -2643,6 +2671,31 @@ virtDBusDomainSetBlockIOTune(GVariant *inArgs,
 }
 
 static void
+virtDBusDomainSetBlockThreshold(GVariant *inArgs,
+                                GUnixFDList *inFDs G_GNUC_UNUSED,
+                                const gchar *objectPath,
+                                gpointer userData,
+                                GVariant **outArgs G_GNUC_UNUSED,
+                                GUnixFDList **outFDs G_GNUC_UNUSED,
+                                GError **error)
+{
+    virtDBusConnect *connect = userData;
+    g_autoptr(virDomain) domain = NULL;
+    const gchar *dev;
+    guint64 threshold;
+    guint flags;
+
+    g_variant_get(inArgs, "(&stu)", &dev, &threshold, &flags);
+
+    domain = virtDBusDomainGetVirDomain(connect, objectPath, error);
+    if (!domain)
+        return;
+
+    if (virDomainSetBlockThreshold(domain, dev, threshold, flags) < 0)
+        virtDBusUtilSetLastVirtError(error);
+}
+
+static void
 virtDBusDomainSetGuestVcpus(GVariant *inArgs,
                             GUnixFDList *inFDs G_GNUC_UNUSED,
                             const gchar *objectPath,
@@ -3220,6 +3273,7 @@ static virtDBusGDBusMethodTable virtDBusDomainMethodTable[] = {
     { "FSThaw", virtDBusDomainFSThaw },
     { "FSTrim", virtDBusDomainFSTrim },
     { "GetBlockIOParameters", virtDBusDomainGetBlockIOParameters },
+    { "GetBlockInfo", virtDBusDomainGetBlockInfo },
     { "GetBlockIOTune", virtDBusDomainGetBlockIOTune },
     { "GetBlockJobInfo", virtDBusDomainGetBlockJobInfo },
     { "GetControlInfo", virtDBusDomainGetControlInfo },
@@ -3273,6 +3327,7 @@ static virtDBusGDBusMethodTable virtDBusDomainMethodTable[] = {
     { "SendProcessSignal", virtDBusDomainSendProcessSignal },
     { "SetBlockIOParameters", virtDBusDomainSetBlockIOParameters },
     { "SetBlockIOTune", virtDBusDomainSetBlockIOTune },
+    { "SetBlockThreshold", virtDBusDomainSetBlockThreshold },
     { "SetGuestVcpus", virtDBusDomainSetGuestVcpus },
     { "SetInterfaceParameters", virtDBusDomainSetInterfaceParameters },
     { "SetVcpus", virtDBusDomainSetVcpus },

commit 08f75417ada427f518a4232236f70f7afb4a6ebf

events: Remove extra white spaces at the end of line
Fixes: 87b16e9ee0df10290603ffdf61458ca44490470e
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

src/events.c
@@ -853,7 +853,7 @@ virtDBusEventsRegister(virtDBusConnect *connect)
 
     virtDBusEventsRegisterDomainEvent(connect,
                                       VIR_DOMAIN_EVENT_ID_BLOCK_THRESHOLD,
-                                      VIR_DOMAIN_EVENT_CALLBACK(virtDBusEventsDomainBlockThreshold));                                      
+                                      VIR_DOMAIN_EVENT_CALLBACK(virtDBusEventsDomainBlockThreshold));
 
     virtDBusEventsRegisterNetworkEvent(connect,
                                        VIR_NETWORK_EVENT_ID_LIFECYCLE,